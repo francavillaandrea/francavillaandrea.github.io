915b04622e6d8398598bfbc1c1766e217b5503ad
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2026-03-25 12:53:23 +0100

    Update

--- app/components/ui/aceternity/index.ts
+++
@@ -1,5 +0,0 @@
-export { TextGenerateEffect } from "./text-generate-effect";
-export { CardHoverEffect } from "./card-hover-effect";
-export { Sparkles } from "./sparkles";
-export { InfiniteMovingCards } from "./infinite-moving-cards";
-export { Timeline } from "./timeline";
